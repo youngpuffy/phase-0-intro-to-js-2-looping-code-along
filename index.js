9c7d1b8ad3f3b48dca5593e2a4b545bef89dc365
youngpuffy <levimuturi2@gmail.com>
2025-03-18 13:30:14 +0300

    learning looping

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const array =["Charlie", "Samip", "Ali"]
+function writeCards(array ,surprise){
+   let me = []
+   for (let i = 0 ; i<array.length ; i++) {
+    let message = `Thank you, ${array[i]}, for the wonderful ${surprise} gift!`    
+    me.push(message);
+   }
+   return me;
+
+}  
+
+function countDown(number){
+   while (number >=0){
+      console.log(number)
+      number --;
+   }
+}
+
+
+ 
+    
+ 
